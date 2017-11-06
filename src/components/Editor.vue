8bfc22d4beae76bf14f3b4f3239764dd0f7d45c3
lucien <1085810633@qq.com>
2017-11-06 16:11:41 +0800

    完成重构了个人信息和工作经历

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -18,24 +18,7 @@
 				<profileEditor v-bind:profile="profile" />
 			</li>
 			<li v-bind:class="{active:currentTab == 1}">
-				<h2>工作经历</h2>
-				<el-form>
-					<div class="wrap" v-for="(wrok,index) in workHistory">
-						<el-form-item label="公 司">
-	    					<el-input v-model="wrok.company" placeholder="请输入公司名称"></el-input>
-	  					</el-form-item>
-	  					<el-form-item label="工作内容">
-	    					<el-input
-							  type="textarea"
-							  :rows="6"
-							  placeholder="请输入内容"
-							  v-model="wrok.content">
-							</el-input>
-	  					</el-form-item>
-	  					<i class="el-icon-error colse" v-on:click="removeWork(index)"></i>
-					</div>
-					<el-button class="addbtn" type="primary" v-on:click="addWork">添加</el-button>	
-				</el-form>
+				<workEditor v-bind:workHistory="workHistory" />
 			</li>
 			<li v-bind:class="{active:currentTab == 2}">
 				<h2>学习经历</h2>
@@ -55,9 +38,11 @@
 
 <script>
 	import ProfileEditor from './profileEditor'
+	import WorkEditor from './workEditor'
 	export default {
 		components:{
-			ProfileEditor
+			ProfileEditor,
+			WorkEditor
 		},
 		data(){
 			return {
@@ -74,14 +59,7 @@
 			}
 		},
 		methods:{
-			addWork(){
-				this.workHistory.push({
-					company: '', content: ''
-				})
-			},
-			removeWork(index){
-				this.workHistory.splice(index,1)
-			}
+			
 		},
 		created(){
 
---
+++ src/components/profileEditor.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h2>个人信息</h2>
+		<el-form class="wrap">
+			<el-form-item label="姓 名">
+				<el-input v-model="profile.name" placeholder="你的姓名"></el-input>
+				</el-form-item>
+				<el-form-item label="城 市">
+				<el-input v-model="profile.city" placeholder="你所在的城市"></el-input>
+				</el-form-item>
+				<el-form-item label="出生年月">
+				<el-input v-model="profile.birth" placeholder="你的出生年/月/日"></el-input>
+				</el-form-item>
+		</el-form>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['profile']
+	}
+</script>
